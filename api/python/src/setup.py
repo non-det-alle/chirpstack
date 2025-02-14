7f2cd811ee19cab703013874e366854b2779daa3
Alessandro Aimi <alleaimi95@gmail.com>
2025-02-14 16:40:53 +0100

    Bump version to 4.9.0+cs.3-test.1

--- api/python/src/setup.py
+++ api/python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "4.9.0+cs.2",
+    version = "4.9.0+cs.3-test.1",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
